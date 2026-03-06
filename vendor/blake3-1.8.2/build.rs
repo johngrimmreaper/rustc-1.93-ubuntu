bd57195f0f489bd5b45c4a69e3a9a69a0afc6be7
Petrichor Park <petrichor.park@canonical.com>
2026-03-06 15:28:56 -0600

    blake3: Use the pure-Rust implementation
    Gbp-Pq: vendor/blake3-no-embedded-c.patch.

--- vendor/blake3-1.8.2/build.rs
+++ vendor/blake3-1.8.2/build.rs
@@ -369,11 +369,13 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     println!("cargo:rerun-if-env-changed=CFLAGS");
 
     // Ditto for source files, though these shouldn't change as often.
-    for file in std::fs::read_dir("c")? {
-        println!(
-            "cargo:rerun-if-changed={}",
-            file?.path().to_str().expect("utf-8")
-        );
+    if !is_pure() {
+        for file in std::fs::read_dir("c")? {
+            println!(
+                "cargo:rerun-if-changed={}",
+                file?.path().to_str().expect("utf-8")
+            );
+        }
     }
 
     // When compiling with clang-cl for windows, it adds .asm files to the root
