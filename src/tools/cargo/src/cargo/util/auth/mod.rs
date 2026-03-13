c4489aab1fd8433f9a5d212dc5d67b1bb3efea7c
Max Gilmour <max.gilmour@canonical.com>
2026-03-13 11:57:29 -0700

    d-0020-remove-windows-dependencies
    Gbp-Pq: prune/d-0020-remove-windows-dependencies.patch.

--- src/tools/cargo/src/cargo/util/auth/mod.rs
+++ src/tools/cargo/src/cargo/util/auth/mod.rs
@@ -564,11 +564,6 @@ fn credential_action(
             }
             "cargo:paseto" => bail!("cargo:paseto requires -Zasymmetric-token"),
             "cargo:token-from-stdout" => Box::new(BasicProcessCredential {}),
-            #[cfg(windows)]
-            "cargo:wincred" => Box::new(cargo_credential_wincred::WindowsCredential {}),
-            #[cfg(target_os = "macos")]
-            "cargo:macos-keychain" => Box::new(cargo_credential_macos_keychain::MacKeychain {}),
-            #[cfg(target_os = "linux")]
             "cargo:libsecret" => Box::new(get_credential_libsecret()?),
             name if BUILT_IN_PROVIDERS.contains(&name) => {
                 Box::new(cargo_credential::UnsupportedCredential {})
