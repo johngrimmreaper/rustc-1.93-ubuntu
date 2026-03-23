f98e9a9fce86d335434e1005630c9a2d0cddf1f7
Max Gilmour <max.gilmour@canonical.com>
2026-03-23 14:54:06 -0700

    remove upstream parts that are not needed for the Debian build, in
    Gbp-Pq: prune/d-0000-ignore-removed-submodules.patch.

--- src/bootstrap/src/core/build_steps/vendor.rs
+++ src/bootstrap/src/core/build_steps/vendor.rs
@@ -21,14 +21,9 @@ pub fn default_paths_to_vendor(builder: &Builder<'_>) -> Vec<(PathBuf, Vec<&'sta
         ("src/tools/cargo/Cargo.toml", vec!["src/tools/cargo"]),
         ("src/tools/clippy/clippy_test_deps/Cargo.toml", vec![]),
         ("src/tools/rust-analyzer/Cargo.toml", vec![]),
-        ("compiler/rustc_codegen_cranelift/Cargo.toml", vec![]),
-        ("compiler/rustc_codegen_gcc/Cargo.toml", vec![]),
         ("library/Cargo.toml", vec![]),
         ("src/bootstrap/Cargo.toml", vec![]),
         ("src/tools/rustbook/Cargo.toml", SUBMODULES_FOR_RUSTBOOK.into()),
-        ("src/tools/rustc-perf/Cargo.toml", vec!["src/tools/rustc-perf"]),
-        ("src/tools/opt-dist/Cargo.toml", vec![]),
-        ("src/doc/book/packages/trpl/Cargo.toml", vec![]),
     ]
     .into_iter()
     .map(|(path, submodules)| (builder.src.join(path), submodules))
